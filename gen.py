9de01989ba8029bd38ddd28856823d858f4acd2d
syskey <83065600+syskey4603@users.noreply.github.com>
2023-05-08 21:34:52 +0530

    fixed erros again

--- gen.py
+++ gen.py
@@ -3,30 +3,30 @@ from datetime import datetime
 import os
 import discord
 
-class gen:
-
-    def getacc(input):
-        BASE_URL = "https://sxskey.pythonanywhere.com"
-        payload = {'input': input}
-        respose = rq.get(BASE_URL, params=payload)
-
-        json_values = respose.json()
-
-        rq_input = json_values['input']
-        timestamp = json_values['timestamp']
 
 
+def getacc(input):
+    BASE_URL = "https://sxskey.pythonanywhere.com"
+    payload = {'input': input}
+    respose = rq.get(BASE_URL, params=payload)
 
-        print(f'input is: {rq_input}')
-        print(f'date is: {datetime.fromtimestamp(timestamp)}')
-        if(input == 'nord'):
-            nordvpnacc = json_values['nordvpnacc']
-            print(f'the account is: {nordvpnacc}')
-            return nordvpnacc
-        elif(input == 'crunchy'):
-            crunchyacc = json_values['crunchyacc']
-            print(f'the account is: {crunchyacc}')
-            return crunchyacc
+    json_values = respose.json()
+
+    rq_input = json_values['input']
+    timestamp = json_values['timestamp']
+
+
+
+    print(f'input is: {rq_input}')
+    print(f'date is: {datetime.fromtimestamp(timestamp)}')
+    if(input == 'nord'):
+        nordvpnacc = json_values['nordvpnacc']
+        print(f'the account is: {nordvpnacc}')
+        return nordvpnacc
+    elif(input == 'crunchy'):
+        crunchyacc = json_values['crunchyacc']
+        print(f'the account is: {crunchyacc}')
+        return crunchyacc
 
 
 
